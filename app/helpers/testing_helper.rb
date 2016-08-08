9e76396c7f76695b2b27b8fc4abe54f5b7c2f249
Richard <hsu.b03723@canon.co.jp>
2016-08-08 13:38:04 +0900

    took out writing to temp file method and replaced it with running system command

--- app/helpers/testing_helper.rb
+++ app/helpers/testing_helper.rb
@@ -1,57 +1,17 @@
 module TestingHelper
 
+  def stringBuilder(original, tag, argument)
+    return original + ' -' + tag + ' ' + argument
+  end
 
-  require "rubypython"
-
-  def runDs(imageFile, ending)
-    Dir.chdir "/home/ub03723/Desktop/caffe-home/caffe_mariolew/examples" do
-
-      RubyPython.start
-
-      main_ruby = RubyPython.import("__main__")
-
-      RubyPython::Python.PyRun_SimpleString <<-PYTHON
-
-import deconvScript as ds
-
-def test_ruby_python(v1, v2):
-  print v1, v2
-  return
-
-def test_ds():
-  return ds.tester()
-
-def run_ds(imageFile, ending):
-  net, t = ds.mainScript(imageFile, ending)
-  net.forward()
-  print 'done forwarding'
-  return str(net.blobs['conv1_1t'].data[0].max())
-
-PYTHON
-      #s = main_ruby.test_ruby_python(imageFile, ending).rubify
-      #s =  main_ruby.run_ds(imageFile, ending).rubify
-
-      RubyPython.stop
-
-      return s
+  def runDsAlt(imageFile, layer)
+    Dir.chdir '/home/ub03723/Desktop/caffe-home/caffe_mariolew/examples' do
+      system_command = 'python deconvScript.py'
+      system_command = stringBuilder(system_command, 'i', '"' + imageFile + '"')
+      system_command = stringBuilder(system_command, 'l', '"' + layer + '"')
+      puts system_command
+      system(system_command)
     end
-
-
-  end ## go back to root directory for this rails app
-
-  def runDsAlt(imageFile, ending)
-    Dir.chdir "/home/ub03723/Desktop/caffe-home/caffe_mariolew/examples" do
-      inputFile = File.open('tempInputFile.txt', 'w')
-      inputFile.puts(imageFile)
-      inputFile.puts(ending)
-      inputFile.puts("")
-      inputFile.close
-
-      system("python /home/ub03723/Desktop/caffe-home/caffe_mariolew/examples/deconvScript.py")
-
-    end
-
-
   end
 
 end
